b4a5ac8e2de954b8cea5021724339ace3ab6ed2a
Charles Blais <charles.blais@canada.ca>
2022-08-04 13:52:05 +0000

    add click

--- setup.py
+++ setup.py
@@ -138,6 +138,7 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
+        'click',
         'pydantic',
         'pydantic[dotenv]',
         'xsdata',
